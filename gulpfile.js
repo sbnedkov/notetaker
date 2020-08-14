bd5c4234e10cf0cb01c5a9cc0c560dece1d3faf6
Svetoslav Nedkov <sveto.n@gmail.com>
2020-08-14 20:07:30 +0300

    Update with npm audit fix; update gulpfile with 4.x

--- gulpfile.js
+++ gulpfile.js
@@ -5,22 +5,23 @@ var bower = require('gulp-bower');
 
 var exec = require('child_process').exec;
 
-gulp.task('dev', function () {
+gulp.task('dev', gulp.series(function () {
     return nodemon({script: 'main.js', ext: 'js', ignore: ['./public'], env: {'NODE_ENV': 'development'}}).on('restart', function () {
         console.log('restart');
     });
-});
+}));
 
-gulp.task('bower', function() {
-    return bower();
-});
+gulp.task('bower', gulp.series(function(cb) {
+    bower();
+    return cb();
+}));
 
-gulp.task('prod', ['bower'], function () {
+gulp.task('prod', gulp.series(['bower'], function () {
     exec('NODE_ENV=production node main.js $PORT', function (error, stdout, stderr) {
         console.log('stdout: ' + stdout);
         console.log('stderr: ' + stderr);
         if (error !== null) {
-          console.log('exec error: ' + error);
+            console.log('exec error: ' + error);
         }
     });
-});
+}));
